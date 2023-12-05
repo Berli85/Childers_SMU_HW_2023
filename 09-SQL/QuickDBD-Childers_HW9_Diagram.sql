dd622a73897ab6a6decf7e2ba3fb71b291d1a1cb
Berli85 <141587077+Berli85@users.noreply.github.com>
2023-12-05 17:55:00 -0600

    Change

---
+++ 09-SQL/QuickDBD-Childers_HW9_Diagram.sql
@@ -0,0 +1,85 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/3MUafA
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "departments" (
+    "dept_num" varchar(10)   NOT NULL,
+    "dept_name" varchar(50)   NOT NULL,
+    "last_updated" timestamp   NOT NULL,
+    CONSTRAINT "pk_departments" PRIMARY KEY (
+        "dept_num"
+     )
+);
+
+CREATE TABLE "titles" (
+    "title_id" varchar(10)   NOT NULL,
+    "title" varchar(50)   NOT NULL,
+    "last_updated" timestamp   NOT NULL,
+    CONSTRAINT "pk_titles" PRIMARY KEY (
+        "title_id"
+     )
+);
+
+CREATE TABLE "employees" (
+    "emp_num" integer   NOT NULL,
+    "emp_title_id" varchar(10)   NOT NULL,
+    "birth_date" datetime   NOT NULL,
+    "first_name" varchar(100)   NOT NULL,
+    "last_name" varchar(100)   NOT NULL,
+    "sex" varchar(5)   NOT NULL,
+    "hire_date" datetime   NOT NULL,
+    "last_updated" timestamp   NOT NULL,
+    CONSTRAINT "pk_employees" PRIMARY KEY (
+        "emp_num"
+     )
+);
+
+CREATE TABLE "salaries" (
+    "salary_id" serial   NOT NULL,
+    "emp_num" integer   NOT NULL,
+    "salary" double   NOT NULL,
+    "last_updated" timestamp   NOT NULL,
+    CONSTRAINT "pk_salaries" PRIMARY KEY (
+        "salary_id"
+     )
+);
+
+CREATE TABLE "dept_emp" (
+    "dept_emp_id" serial   NOT NULL,
+    "emp_num" integer   NOT NULL,
+    "dept_num" varchar(10)   NOT NULL,
+    "last_updated" timestamp   NOT NULL,
+    CONSTRAINT "pk_dept_emp" PRIMARY KEY (
+        "dept_emp_id"
+     )
+);
+
+CREATE TABLE "dept_manager" (
+    "dept_manager_id" serial   NOT NULL,
+    "dept_num" varchar(10)   NOT NULL,
+    "emp_num" integer   NOT NULL,
+    "last_updated" timestamp   NOT NULL,
+    CONSTRAINT "pk_dept_manager" PRIMARY KEY (
+        "dept_manager_id"
+     )
+);
+
+ALTER TABLE "departments" ADD CONSTRAINT "fk_departments_dept_num" FOREIGN KEY("dept_num")
+REFERENCES "dept_manager" ("dept_num");
+
+ALTER TABLE "titles" ADD CONSTRAINT "fk_titles_title_id" FOREIGN KEY("title_id")
+REFERENCES "employees" ("emp_title_id");
+
+ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_num" FOREIGN KEY("emp_num")
+REFERENCES "dept_emp" ("emp_num");
+
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_num" FOREIGN KEY("emp_num")
+REFERENCES "employees" ("emp_num");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_num" FOREIGN KEY("dept_num")
+REFERENCES "dept_manager" ("dept_num");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_num" FOREIGN KEY("emp_num")
+REFERENCES "employees" ("emp_num");
+
